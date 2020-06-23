27b8bfdc3b3814b21af13553f0533c2de4afbe62
ds <sthy14@icloud.com>
2020-06-23 12:43:39 +0100

    done more exe

--- exercises/test4.rs
+++ exercises/test4.rs
@@ -5,7 +5,15 @@
 
 // Write a macro that passes the test! No hints this time, you can do it!
 
-// I AM NOT DONE
+mod macros {
+    #[macro_export]
+    macro_rules! my_macro {
+        ($msg:expr) => {
+            "Hello ".to_string() + $msg;
+        };
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
